9ef48da55fecb92b676ed83e8fa61d858414d6d8
Piotr Mińkowski <piotr_minkowski@wp.pl>
2018-04-11 23:14:41 +0200

    Test version

--- caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
+++ caller-service/src/main/java/pl/piomin/services/caller/CallerApplication.java
@@ -2,6 +2,8 @@ package pl.piomin.services.caller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CallerApplication {
@@ -10,4 +12,9 @@ public class CallerApplication {
 		SpringApplication.run(CallerApplication.class, args);
 	}
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
--- callme-service/src/main/java/pl/piomin/services/caller/controller/CallmeController.java
+++ callme-service/src/main/java/pl/piomin/services/caller/controller/CallmeController.java
@@ -5,21 +5,17 @@ import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/caller")
-public class CallerController {
+@RequestMapping("/callme")
+public class CallmeController {
 
 	@Autowired
 	BuildProperties buildProperties;
-	@Autowired
-	RestTemplate restTemplate;
 	
 	@GetMapping("/ping")
 	public String ping() {
-		String response = restTemplate.getForObject("http://callme-service/callme/ping", String.class);
-		return buildProperties.getName() + ":" + buildProperties.getVersion() + ". Calling... " + response; 
+		return buildProperties.getName() + ":" + buildProperties.getVersion(); 
 	}
 	
 }
