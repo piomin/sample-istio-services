fd5a36186fdc438ba988aec8b263df337a73773a
piomin <piotr.minkowski@gmail.com>
2020-05-29 23:58:12 +0200

    Fix formatting

--- caller-service/src/main/java/pl/piomin/services/caller/controller/CallerController.java
+++ caller-service/src/main/java/pl/piomin/services/caller/controller/CallerController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,38 +15,38 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/caller")
 public class CallerController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CallerController.class);
-	
-	@Autowired
-	BuildProperties buildProperties;
-	@Autowired
-	RestTemplate restTemplate;
-	@Value("${VERSION}")
-	private String version;
-	
-	@GetMapping("/ping")
-	public String ping() {
-		LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), version);
-		String response = restTemplate.getForObject("http://callme-service:8080/callme/ping", String.class);
-		LOGGER.info("Calling: response={}", response);
-		return "I'm caller-service " + version + ". Calling... " + response;
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallerController.class);
 
-	@GetMapping("/ping-with-random-error")
-	public ResponseEntity<String> pingWithRandomError() {
-		LOGGER.info("Ping with random error: name={}, version={}", buildProperties.getName(), version);
-		ResponseEntity<String> responseEntity =
-				restTemplate.getForEntity("http://callme-service:8080/callme/ping-with-random-error", String.class);
-		LOGGER.info("Calling: responseCode={}, response={}", responseEntity.getStatusCode(), responseEntity.getBody());
-		return new ResponseEntity<>("I'm caller-service " + version + ". Calling... " + responseEntity.getBody(), responseEntity.getStatusCode());
-	}
+    @Autowired
+    BuildProperties buildProperties;
+    @Autowired
+    RestTemplate restTemplate;
+    @Value("${VERSION}")
+    private String version;
 
-	@GetMapping("/ping-with-random-delay")
-	public String pingWithRandomDelay() {
-		LOGGER.info("Ping with random delay: name={}, version={}", buildProperties.getName(), version);
-		String response = restTemplate.getForObject("http://callme-service:8080/callme/ping-with-random-delay", String.class);
-		LOGGER.info("Calling: response={}", response);
-		return "I'm caller-service " + version + ". Calling... " + response;
-	}
+    @GetMapping("/ping")
+    public String ping() {
+        LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), version);
+        String response = restTemplate.getForObject("http://callme-service:8080/callme/ping", String.class);
+        LOGGER.info("Calling: response={}", response);
+        return "I'm caller-service " + version + ". Calling... " + response;
+    }
+
+    @GetMapping("/ping-with-random-error")
+    public ResponseEntity<String> pingWithRandomError() {
+        LOGGER.info("Ping with random error: name={}, version={}", buildProperties.getName(), version);
+        ResponseEntity<String> responseEntity =
+                restTemplate.getForEntity("http://callme-service:8080/callme/ping-with-random-error", String.class);
+        LOGGER.info("Calling: responseCode={}, response={}", responseEntity.getStatusCode(), responseEntity.getBody());
+        return new ResponseEntity<>("I'm caller-service " + version + ". Calling... " + responseEntity.getBody(), responseEntity.getStatusCode());
+    }
+
+    @GetMapping("/ping-with-random-delay")
+    public String pingWithRandomDelay() {
+        LOGGER.info("Ping with random delay: name={}, version={}", buildProperties.getName(), version);
+        String response = restTemplate.getForObject("http://callme-service:8080/callme/ping-with-random-delay", String.class);
+        LOGGER.info("Calling: response={}", response);
+        return "I'm caller-service " + version + ". Calling... " + response;
+    }
 
 }
--- callme-service/src/main/java/pl/piomin/services/callme/controller/CallmeController.java
+++ callme-service/src/main/java/pl/piomin/services/callme/controller/CallmeController.java
@@ -18,41 +18,41 @@ import java.util.UUID;
 @RequestMapping("/callme")
 public class CallmeController {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(CallmeController.class);
-	private static final String INSTANCE_ID = UUID.randomUUID().toString();
-	private Random random = new Random();
+    private static final Logger LOGGER = LoggerFactory.getLogger(CallmeController.class);
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private Random random = new Random();
 
-	@Autowired
-	BuildProperties buildProperties;
-	@Value("${VERSION}")
-	private String version;
-	
-	@GetMapping("/ping")
-	public String ping() {
-		LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), version);
-		return "I'm callme-service " + version;
-	}
+    @Autowired
+    BuildProperties buildProperties;
+    @Value("${VERSION}")
+    private String version;
 
-	@GetMapping("/ping-with-random-error")
-	public ResponseEntity<String> pingWithRandomError() {
-		int r = random.nextInt(100);
-		if (r%2 == 0) {
-			LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
-					buildProperties.getName(), version, r, HttpStatus.GATEWAY_TIMEOUT);
-			return new ResponseEntity<>("Surprise " + INSTANCE_ID + " " + version, HttpStatus.GATEWAY_TIMEOUT);
-		} else {
-			LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
-					buildProperties.getName(), version, r, HttpStatus.OK);
-			return new ResponseEntity<>("I'm callme-service" + INSTANCE_ID + " " + version, HttpStatus.OK);
-		}
-	}
+    @GetMapping("/ping")
+    public String ping() {
+        LOGGER.info("Ping: name={}, version={}", buildProperties.getName(), version);
+        return "I'm callme-service " + version;
+    }
 
-	@GetMapping("/ping-with-random-delay")
-	public String pingWithRandomDelay() throws InterruptedException {
-		int r = new Random().nextInt(3000);
-		LOGGER.info("Ping with random delay: name={}, version={}, delay={}", buildProperties.getName(), version, r);
-		Thread.sleep(r);
-		return "I'm callme-service " + version;
-	}
-	
+    @GetMapping("/ping-with-random-error")
+    public ResponseEntity<String> pingWithRandomError() {
+        int r = random.nextInt(100);
+        if (r % 2 == 0) {
+            LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
+                    buildProperties.getName(), version, r, HttpStatus.GATEWAY_TIMEOUT);
+            return new ResponseEntity<>("Surprise " + INSTANCE_ID + " " + version, HttpStatus.GATEWAY_TIMEOUT);
+        } else {
+            LOGGER.info("Ping with random error: name={}, version={}, random={}, httpCode={}",
+                    buildProperties.getName(), version, r, HttpStatus.OK);
+            return new ResponseEntity<>("I'm callme-service" + INSTANCE_ID + " " + version, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/ping-with-random-delay")
+    public String pingWithRandomDelay() throws InterruptedException {
+        int r = new Random().nextInt(3000);
+        LOGGER.info("Ping with random delay: name={}, version={}, delay={}", buildProperties.getName(), version, r);
+        Thread.sleep(r);
+        return "I'm callme-service " + version;
+    }
+
 }
