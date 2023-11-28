99e8ff5a4b02da8338b9ce7ac9fbdb290901a730
piomin <piotr.minkowski@gmail.com>
2023-11-28 11:58:20 +0100

    fix k6 thresholds

--- first-service/src/test/resources/k6/load-test.js
+++ first-service/src/test/resources/k6/load-test.js
@@ -2,10 +2,10 @@ import { sleep } from 'k6';
 import http from 'k6/http';
 
 export const options = {
-  duration: '30s',
+  duration: '60s',
   vus: 10,
   thresholds: {
-    http_req_failed: ['rate<0.2'],
+    http_req_failed: ['rate<0.25'],
     http_req_duration: ['p(95)<1000'],
   },
 };
