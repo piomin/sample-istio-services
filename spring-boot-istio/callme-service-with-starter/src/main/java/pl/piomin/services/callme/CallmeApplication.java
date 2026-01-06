66f250e2e8ed9a0efb734d9b54a7355a146faf5f
piomin <piotr.minkowski@gmail.com>
2026-01-06 10:18:59 +0100

    Update to the latest spring boot istio 1.2.2

--- spring-boot-istio/callme-service-with-starter/src/main/java/pl/piomin/services/callme/CallmeApplication.java
+++ spring-boot-istio/callme-service-with-starter/src/main/java/pl/piomin/services/callme/CallmeApplication.java
@@ -2,11 +2,14 @@ package pl.piomin.services.callme;
 
 import com.github.piomin.springboot.istio.annotation.EnableIstio;
 import com.github.piomin.springboot.istio.annotation.Fault;
+import com.github.piomin.springboot.istio.annotation.Match;
+import com.github.piomin.springboot.istio.annotation.MatchType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableIstio(enableGateway = true)
+@EnableIstio(enableGateway = true, version = "v1",
+	matches = {@Match(type = MatchType.HEADERS, key = "X-Version", value = "v1")})
 public class CallmeApplication {
 
 	public static void main(String[] args) {
